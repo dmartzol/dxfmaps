fe899baf87725cf1baa1e5e35b1e5778e4b85580
Daniel Martinez Olivas <danielmartinezolivas@gmail.com>
2018-12-05 15:19:47 -0600

    WIP

--- map2svg/application.py
+++ map2svg/application.py
@@ -1,19 +1,43 @@
 from map2svg import names
+from map2svg.utils import project, filter_by_area, dataframe2polygons, simplify, save_svg
 import osmnx as ox
 import shapely
+import geopandas as gpd
+import os
+
 
 def main():
-    ox.config(use_cache=True)
-    comunidades_gdf = ox.gdf_from_places(names.CCAA)
-    for index, row in comunidades_gdf.iterrows():
-        if isinstance(row.geometry, shapely.geometry.multipolygon.MultiPolygon):
-            multipolygon = row.geometry
-            areas = [poligon.area for poligon in multipolygon.geoms]
-            maximum_area = max(areas)
-            max_area_polygon = row.geometry.geoms[areas.index(maximum_area)]
-            comunidades_gdf.iloc[index,4] = max_area_polygon
-    # project the geometry to the appropriate UTM zone
-    comunidades_gdf = ox.project_gdf(comunidades_gdf)
-    polygons = []
-    for index, row in comunidades_gdf.iterrows():
-        polygons.append(row.geometry)
+    f = os.path.join("shapefiles", "cb_2017_us_state_500k", "cb_2017_us_state_500k.shp")
+    shapefile = gpd.read_file(f)
+    for i, row in shapefile.iterrows():
+        if row.NAME not in names.USA_STATES:
+            shapefile.drop(index = i, inplace = True)
+    states_gdf_projection = project(shapefile)
+    polygons = dataframe2polygons(states_gdf_projection)
+
+    print("States in list: {}".format(len(names.USA_STATES)))
+    print("Polygons before filtering by area: {}".format(len(polygons)))
+    polygons = filter_by_area(polygons, area_thresold = 1000000000)
+    print("Polygons after filtering by area: {}".format(len(polygons)))
+    
+    simplify(polygons, tolerance = 5000)
+
+    # Translating the map to the origin
+    full_map = shapely.geometry.MultiPolygon(polygons)
+    offset_x = - min(full_map.bounds[0], full_map.bounds[2])
+    offset_y = - min(full_map.bounds[1], full_map.bounds[3])
+    full_map = shapely.affinity.translate(full_map, xoff=offset_x, yoff=offset_y)
+
+    # Scaling the polygons to reduce their size
+    factor = 90 / max(full_map.bounds)
+    factor = 1.91787670701e-05
+    print("")
+    print("Scaling factor: {}".format(factor))
+    print("Old bounds: {}".format(full_map.bounds))
+    full_map = shapely.affinity.scale(full_map, xfact=factor, yfact=factor, origin=(0, 0))
+    print("New bounds: {}".format(full_map.bounds))
+
+    save_svg(full_map, size="500", units="px")
+    interior = full_map.buffer(0.5, cap_style=2, join_style=1)
+    interior = interior.buffer(-1.0, cap_style=2, join_style=1)
+    save_svg(interior, filename='out_buffered.svg', size="500", units="px")
---
+++ map2svg/utils.py
@@ -0,0 +1,71 @@
+import osmnx as ox
+import shapely
+
+def fetch(list_of_names):
+    ox.config(use_cache=True)
+    geodataframe = ox.gdf_from_places(list_of_names)
+    return geodataframe
+
+def project(geodataframe):
+    return ox.project_gdf(geodataframe)
+
+
+def dataframe2polygons(geodataframe):
+    polygons = []
+    for index, row in geodataframe.iterrows():
+        if isinstance(row.geometry, shapely.geometry.multipolygon.MultiPolygon):
+            for geom in row.geometry.geoms:
+                polygons.append(geom)
+        elif isinstance(row.geometry, shapely.geometry.polygon.Polygon):
+            polygons.append(row.geometry)
+        elif isinstance(row.geometry, shapely.geometry.linestring.LineString):
+            print("")
+            print("{} geometry is LineString".format(row.place_name))
+            print("This linestring is ring: {}".format(row.geometry.is_ring))
+            # TODO: Convert linestring to polygon
+            # polygons.append(row.geometry)
+        else:
+            raise Exception('Found non valid geometry')
+    return polygons
+
+def filter_by_area(polygons, area_thresold = 1000000000):
+    # TODO: Figure units for area!
+    return [polygon for polygon in polygons if polygon.area > area_thresold]
+
+def simplify(polygons, tolerance = 2000, verbose = False):
+    # TODO: calculate adequate tolerance
+    for i, polygon in enumerate(polygons):
+        polygons[i] = shapely.geometry.Polygon(polygon.exterior.coords)
+        nodes_before = len(list(polygon.exterior.coords))
+        polygons[i] = polygons[i].simplify(tolerance=tolerance)
+        if verbose:
+            nodes = len(list(polygons[i].exterior.coords))
+            print("Polygon nodes reduced by {:.1f}%, from {} to {}".format(
+                100*(nodes_before-nodes)/float(nodes_before),
+                nodes_before,
+                nodes
+                )
+            )
+
+def save_svg(full_map, filename='out.svg', stroke_width=0.1, size="500", units="px"):
+    size_and_units = "{}{}".format(size, units)
+    poligon_template = "\n<polygon stroke=\"black\" stroke-width=\"{}\" fill=\"none\" points=\"{} \"/>\n"
+    file = open(filename, 'w')
+    svg_style = "<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" width=\"{}\" height=\"{}\" viewBox=\"{} {} {} {}\"> \n"
+    file.write(svg_style.format(size_and_units, size_and_units, 0, 0, full_map.bounds[2], full_map.bounds[3]))
+    file.write("<g transform=\"translate(0,{}) scale(1,-1)\">\n".format(full_map.bounds[3]))
+    list_of_coords = []
+    if isinstance(full_map, shapely.geometry.multipolygon.MultiPolygon):
+        for polygon in full_map.geoms:
+            for x, y in polygon.exterior.coords:
+                list_of_coords.append("{},{}".format(x, y))
+            file.write(poligon_template.format(stroke_width, " ".join(list_of_coords)))
+            list_of_coords = []
+    if isinstance(full_map, shapely.geometry.polygon.Polygon):
+        polygon = full_map
+        for x, y in polygon.exterior.coords:
+            list_of_coords.append("{},{}".format(x, y))
+        file.write(poligon_template.format(stroke_width, " ".join(list_of_coords)))
+    file.write("</g>\n")
+    file.write("</svg>")
+    file.close()
