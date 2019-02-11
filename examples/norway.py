4070c95017c54a77a3a956a82d5cbe2fc7bec048
Daniel Martinez Olivas <danielmartinezolivas@gmail.com>
2019-02-10 21:47:11 -0600

    Same like previous commit

---
+++ examples/norway.py
@@ -0,0 +1,23 @@
+import dxfmaps
+import shapefile
+import sys
+from dxfmaps.projections import mercator
+
+VERBOSE = False
+
+
+def main():
+    sf = shapefile.Reader(dxfmaps.utils.WORLD_COUNTRIES)
+    map = dxfmaps.Map(sf, countries=['spain', 'norway'])
+    map.filter_by_area(area_thresold=.5)
+    map.project('mercator')
+    map.simplify(tolerance=.005)
+    map.translate_to_center()
+    map.scale_to_width()
+    map.add_names()
+    map.to_svg(filename='norway.svg')
+    map.to_dxf(filename='norway.dxf')
+    # print(map.multipolygon.centroid)
+
+if __name__ == "__main__":
+    main()
