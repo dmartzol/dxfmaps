f012982de858fa2f32aee6533254623efbab2cb7
Daniel Martinez Olivas <danielmartinezolivas@gmail.com>
2019-02-26 14:01:27 -0600

    Fixed function to find boxes into countries and added comments

--- examples/countries.py
+++ examples/countries.py
@@ -10,21 +10,21 @@ def main():
     sf = shapefile.Reader(dxfmaps.utils.WORLD_COUNTRIES)
     list_of_countries = [
         'norway',
-        # 'spain',
-        # 'france',
-        # 'italy',
-        # 'germany',
-        # 'Denmark',
-        # 'Poland',
-        # 'sweden'
+        'spain',
+        'france',
+        'italy',
+        'germany',
+        'Denmark',
+        'Poland',
+        'sweden'
     ]
     map = dxfmaps.Map(sf, countries=list_of_countries)
     map.filter_by_area(area_thresold=.5)
     map.project('mercator')
     map.simplify(tolerance=.015)
-    map.add_names()
     map.translate_to_center()
     map.scale_to_width()
+    map.add_names()
     map.to_svg(filename='countries.svg')
     map.to_dxf(filename='countries.dxf')
     # print(map.multipolygon.centroid)
